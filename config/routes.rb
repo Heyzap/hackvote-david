8b69244819afc47013db21d574355764d7137c0b
David Motsonashvili <davidmotson@gmail.com>
2014-05-28 16:31:07 -0700

    Finished, hopefully

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 HackdayVoting::Application.routes.draw do
+  resources :hack_days, only: [:create, :new, :show]
+  resources :projects, only: [:update]
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
